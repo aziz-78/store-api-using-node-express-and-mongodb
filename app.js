ab29ab56d2d58e20730044130ef66f04036b8b47
aziz_78 <aziz.fre9@gmail.com>
2023-09-08 10:57:24 +0500

    connected to db

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('04 Store API')
+
+require("dotenv").config()
+const express = require("express")
+const app = express()
+
+const notfoundmiddleware = require("./middleware/not-found")
+const errorhandlermiddleware = require("./middleware/error-handler")
+const connectDB = require("./db/connect")
+app.use(express.json())
+app.use(notfoundmiddleware)
+app.use(errorhandlermiddleware)
+const port = process.env.port || 3000
+const start = async()=>{
+try {
+    await connectDB(process.env.db_url);
+    app.listen(port,()=>{
+        console.log(`server is running on port ${port}`)
+    }) 
+} catch (error) {
+    console.log(error)
+}
+}
+start()
+
--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
+const connectDB = async(url) => {
+   mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
+  console.log("connected")
 }
 
 module.exports = connectDB
