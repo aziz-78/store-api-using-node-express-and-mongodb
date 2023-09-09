6982f40ee3239f6e276e38ef0b5b15f2ed1bbce1
aziz_78 <aziz.fre9@gmail.com>
2023-09-10 00:55:52 +0500

    added pagination

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,13 @@ const Product = require("../models/product")
 
 const getAllProductsStatic = async(req,res) =>{
     const search = "ab"
-    const prod = await Product.find({name:{$regex:search,$options:'i'}})
+    const prod = await Product.find({name:{$regex:search,$options:'i'}}).limit(5).skip(1)
     return res.status(200).json({prod,nbHits:prod.length})
 }
 const getAllProducts = async(req,res)=>{
     const queryobj = {}
-    const {featured,name,company} = req.query
+    
+    const {featured,name,company,sort,fields} = req.query
     if(featured){
         queryobj.featured = featured === "true" ? true : false
     }
@@ -17,7 +18,22 @@ const getAllProducts = async(req,res)=>{
     if(company){
         queryobj.company = company
     }
-    const prod = await Product.find(queryobj)
+    const result = Product.find(queryobj)
+    if(sort){
+       const sortedlist = sort.split(",").join(" ")
+       result.sort(sortedlist)
+    }
+    else{
+        result.sort("createdAt")
+    }
+    if(fields){
+        const fieldlist = fields.split(",").join(" ")
+       result.select(fieldlist)
+    }
+    const page =Number(req.query.page) || 1
+    const limit =Number(req.query.limit) || 10
+    const skip = (page -1) * limit
+    const prod = await result.limit(limit).skip(skip)
     res.status(200).json({prod})
 }
 
