3d4b23e18231c43a3c25d26f5a3d4f33ba52f887
aziz_78 <aziz.fre9@gmail.com>
2023-09-14 05:35:44 +0500

    edited static response

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,12 @@
 const Product = require("../models/product")
 
-const getAllProductsStatic = async(req,res) =>{
-    const search = "ab"
-    const prod = await Product.find({price:{$gt:30,$lt:300}}).limit(5).skip(1)
-    return res.status(200).json({prod,nbHits:prod.length})
-}
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({ price: { $gt: 30 } })
+      .sort('price')
+      .select('name price');
+  
+    res.status(200).json({ products, nbHits: products.length });
+  };
 const getAllProducts = async(req,res)=>{
     const queryobj = {}
     
